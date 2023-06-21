d285fc4281704702390870f7d397997ccb38d065
Noel Samuel <noes14155@gmail.com>
2023-06-22 03:33:01 +0400

    Change lang

--- bot/botdb.py
+++ bot/botdb.py
@@ -12,7 +12,7 @@ class Database:
 
     def create_tables(self):
         settings_query = '''CREATE TABLE IF NOT EXISTS settings 
-             (user_id INTEGER PRIMARY KEY, api_name TEXT, model TEXT)'''
+             (user_id INTEGER PRIMARY KEY, lang TEXT)'''
         history_query = '''CREATE TABLE IF NOT EXISTS history 
              (user_id INTEGER, role TEXT, content TEXT)'''
         self.conn.execute(settings_query)
@@ -23,19 +23,15 @@ class Database:
         if self.conn:
             self.conn.close()
 
-    def insert_settings(self, user_id, api_name, model):
-        query = '''INSERT OR IGNORE INTO settings (user_id, api_name, model)
+    def insert_settings(self, user_id, lang):
+        query = '''INSERT OR IGNORE INTO settings (user_id, lang)
                  VALUES (?, ?, ?)'''
-        self.conn.execute(query, (user_id, api_name, model))
+        self.conn.execute(query, (user_id, lang))
         self.conn.commit()
 
-    def update_settings(self, user_id, api_name=None, model=None):
-        if api_name:
-            query = '''UPDATE settings SET api_name=? WHERE user_id=?'''
-            self.conn.execute(query, (api_name, user_id))
-        if model:
-            query = '''UPDATE settings SET model=? WHERE user_id=?'''
-            self.conn.execute(query, (model, user_id))
+    def update_settings(self, user_id, lang=None):
+        query = '''UPDATE settings SET lang=? WHERE user_id=?'''
+        self.conn.execute(query, (lang, user_id))
         self.conn.commit()
 
     def insert_history(self, user_id, role, content):
@@ -45,11 +41,13 @@ class Database:
         self.conn.commit()
 
     def get_settings(self, user_id):
-        query = '''SELECT api_name, model FROM settings WHERE user_id=?'''
+        query = '''SELECT lang FROM settings WHERE user_id=?'''
         row = self.conn.execute(query, (user_id,)).fetchone()
         if row:
-            api_name, model = row
-            return api_name, model
+            lang = row[0]
+            return lang
+        else:
+            return None
 
     def get_history(self, user_id):
         query = '''SELECT role, content FROM history WHERE user_id=?'''
